78e5200bc9c338cbea266c5218102ef5f21a9eeb
Samyam-Sapkota <samyamsapkota@gmail.com>
2025-06-06 12:30:17 +0545

    used .env to store secrets

--- main/settings.py
+++ main/settings.py
@@ -10,18 +10,18 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from decouple import Config, Csv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+config = Config(repository='.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ldle+z)od3$jh-@b@h6@!li61^+$#it%5ejjvykua&ty#9ug7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
